c14f5e21cb864d6c2b6bc29266d9982d68ce7573
Jual Rana <contactjual@gmail.com>
2024-12-09 18:23:45 +0600

    first commit

---
+++ more-javascript.js
@@ -0,0 +1,19 @@
+/** 
+
+javaScript is ----
+
+1. JIT Compiled
+2. Multi Paradigm
+3. Prototyped-based
+4. Dynamically-typed
+5. High Abstraction 
+6. Garbage Collection
+
+
+
+
+javaScript language show output by v8 engine
+
+*/
+
+
